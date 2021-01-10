39ffdbf787f58cade0a83532a44c1aa28fce800c
Scolaro, John (Data61, Pullenvale) <John.Scolaro@data61.csiro.au>
2021-01-10 15:25:17 +1000

    day 6 - Done! :D

---
+++ day5/day5.py
@@ -0,0 +1,19 @@
+'''
+Day 5 of the advent of code problems.
+'''
+
+if __name__ == "__main__":
+    boarding_ids = []
+    with open('input.txt', 'r') as fp:
+        for line in fp:
+            boarding_ids.append(int(line.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2))
+    
+    # Part A
+    print(max(boarding_ids))
+    
+    # Part B
+    boarding_ids.sort()
+    for i in range(len(boarding_ids)):
+        if boarding_ids[i + 1] - boarding_ids[i] == 2:
+            print(boarding_ids[i] + 1)
+            exit(0)
