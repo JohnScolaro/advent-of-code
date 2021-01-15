f935f959c9b29584445b77f7dcc121cbbbdb46c3
Scolaro, John (Data61, Pullenvale) <John.Scolaro@data61.csiro.au>
2021-01-15 21:07:41 +1000

    Adding templates for future days

---
+++ day17/day17.py
@@ -0,0 +1,8 @@
+'''
+Solutions for the Advent of Code - Day 17
+'''
+
+if __name__ == "__main__":
+    with open('input.txt', 'r') as fb:
+        for line in fb:
+            print(line)
---
+++ day23/day23.py
@@ -0,0 +1,14 @@
+'''
+Solutions for the Advent of Code - Day 23
+'''
+
+def read_input(file_name: str):
+    l = []
+    with open(file_name, 'r') as fb:
+        for line in fb:
+            l.append(line.strip())
+    return l
+
+if __name__ == "__main__":
+    l = read_input('input.txt')
+    print(l)
