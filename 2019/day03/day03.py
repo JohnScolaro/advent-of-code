7117ce921da89ffe3cf5ceb795e1c9a36541c73f
Scolaro, John (Data61, Pullenvale) <John.Scolaro@data61.csiro.au>
2021-03-14 16:20:26 +1000

    Day3 Solutions Added

--- 2019/day03/day03.py
+++ 2019/day03/day03.py
@@ -2,6 +2,8 @@
 Problem 3 of the Advent-of-Code 2019
 """
 
+from collections import defaultdict
+
 def read_inputs(filename: str):
     l = []
     with open(filename, 'r') as fp:
@@ -15,15 +17,22 @@ def get_instructions_from_string(string_of_instructions: str) -> list:
     """
     return [(x[0], int(x[1:])) for x in string_of_instructions.split(',')]
 
-def get_all_coords_with_wire_on_them(list_of_instructions: list) -> set:
+def get_all_coords_with_wire_on_them(list_of_instructions: list) -> dict:
     """
-    It is easy to find crossings if we just simply have sets of all the
-    coordinates the wire exists at. Then we can simply find the points that
-    exist in multiple sets. This function returns a set of all the coordinates
-    this wire exists at if we follow the instructions from the origin point.
+    Takes a list of instructions, generates the locations of the lines, and
+    stores them in a dictionary. The keys are the locations of wire (as a
+    tuple) and the values are a list of times at which the wire was at those
+    points.
+
+    Params:
+        list_of_instruction (list): The instructions are tuples like: ('U', 3)
+
+    Returns:
+        dict of locations the line is at.
     """
-    wire_positions = set()
+    wire_positions = defaultdict(list)
     loc = (0, 0)
+    time = 0
     for instruction in list_of_instructions:
         direction = instruction[0]
         distance = instruction[1]
@@ -36,25 +45,19 @@ def get_all_coords_with_wire_on_them(list_of_instructions: list) -> set:
         elif direction == 'D':
             increment = (0, -1)
         for i in range(distance):
-            wire_positions.add((loc[0] + (i * increment[0]), loc[1] + (i * increment[1])))
+            wire_positions[(loc[0] + (i * increment[0]), loc[1] + (i * increment[1]))].append(time)
+            time += 1
         loc = (loc[0] + (distance * increment[0]), loc[1] + (distance * increment[1]))
-    wire_positions.add(loc)
-    return wire_positions
-
-def steps_to_get_to_point(list_of_instructions: list, point: tuple) -> int:
-    """
-    
-    """
-    pass
-
+    wire_positions[loc].append(time)
+    return dict(wire_positions)
 
 def part_a(wire_paths: list):
     """
     Find all the crossings, then return the manhattan distance of the crossing
     with the smallest manhattan distance to the origin point.
     """
-    wire_1_locs = get_all_coords_with_wire_on_them(get_instructions_from_string(wire_paths[0]))
-    wire_2_locs = get_all_coords_with_wire_on_them(get_instructions_from_string(wire_paths[1]))
+    wire_1_locs = set(get_all_coords_with_wire_on_them(get_instructions_from_string(wire_paths[0])).keys())
+    wire_2_locs = set(get_all_coords_with_wire_on_them(get_instructions_from_string(wire_paths[1])).keys())
     crossings = wire_1_locs.intersection(wire_2_locs)
     crossings.remove((0, 0)) # Remove origin point
     crossings = list(crossings)
@@ -64,8 +67,21 @@ def part_a(wire_paths: list):
     
 def part_b(wire_paths: list):
     """
+    Find all the crossings, but at the total combined 'time' for the signal to
+    get to the crossing.
     """
-    pass
+    wire_1_dict = get_all_coords_with_wire_on_them(get_instructions_from_string(wire_paths[0]))
+    wire_2_dict = get_all_coords_with_wire_on_them(get_instructions_from_string(wire_paths[1]))
+    wire_1_locs = set(wire_1_dict.keys())
+    wire_2_locs = set(wire_2_dict.keys())
+    crossings = wire_1_locs.intersection(wire_2_locs)
+    crossings.remove((0, 0)) # Remove origin point
+    crossings = list(crossings)
+    list_of_combined_times = []
+    for crossing in crossings:
+        for i in range(len(wire_1_dict[crossing])):
+            list_of_combined_times.append(wire_1_dict[crossing][i] + wire_2_dict[crossing][i])
+    return min(list_of_combined_times)
 
 if __name__ == "__main__":
     l = read_inputs('input.txt')
