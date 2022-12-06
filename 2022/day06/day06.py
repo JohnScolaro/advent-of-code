65517420d7221951b1d1ef9032e9e5ba888a7e09
john-scolaro <john.scolaro@polymathian.com>
2022-12-06 15:57:03 +1000

    clean up day 6 for fun

--- 2022/day06/day06.py
+++ 2022/day06/day06.py
@@ -2,38 +2,22 @@
 Problem 5 of the Advent-of-Code 2022
 """
 
-from typing import Any, List
 import more_itertools
-from collections import Counter
 
 
-def read_inputs(filename: str) -> List[Any]:
-    input_text = []
+def read_inputs(filename: str) -> str:
     with open(filename, "r") as fp:
         for line in fp:
-            input_text.append(line.strip())
-    return input_text
+            return line.strip()
 
 
-def part_a(input_list: List[int]) -> int:
-    a = 0
-    for i, j, k, l in more_itertools.windowed(input_list[0], 4):
-        if len(set([i, j, k, l])) == 4:
-            return a + 4
-        else:
-            a += 1
-
-
-def part_b(input_list: List[int]) -> int:
-    xx = 0
-    for a, b, c, d, e, f, g, h, i, j, k, l, m, n in more_itertools.windowed(input_list[0], 14):
-        if len(set([a, b, c, d, e, f, g, h, i, j, k, l, m, n])) == 14:
-            return xx + 14
-        else:
-            xx += 1
+def part_x(input: str, window_length: int) -> int:
+    for i, b in enumerate(more_itertools.windowed(input, window_length)):
+        if len(set(b)) == window_length:
+            return i + window_length
 
 
 if __name__ == "__main__":
-    input_list = read_inputs("input.txt")
-    print(f"Part A: {part_a(input_list)}")
-    print(f"Part B: {part_b(input_list)}")
+    input = read_inputs("input.txt")
+    print(f"Part A: {part_x(input, 4)}")
+    print(f"Part B: {part_x(input, 14)}")
